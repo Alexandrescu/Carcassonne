2ba4d2363ce8ca61b07726df8cf7b594ebd3b9d5
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-01-30 14:40:27 +0000

    Testing State

--- Game/src/main/scala/State.scala
+++ Game/src/main/scala/State.scala
@@ -1,10 +1,22 @@
 package main.scala
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+
+import scala.collection.immutable.HashMap
+
 /**
  * Created by Andrei on 25/01/15.
  */
-object State {
-  def main(args : Array[String]): Unit = {
-    println("This is working")
+
+class State(private val tileBag : TileBag) {
+  private var board : Map[(Integer, Integer), Tile] = new HashMap[(Integer, Integer), Tile]
+  board = board + (((0, 0), tileBag.startTile))
+
+  def getBoard() = board
+
+  def playMove(): Unit = {
+    throw new NotImplementedException()
   }
+
+
 }
--- game/src/test/scala/StateTest.scala
+++ game/src/test/scala/StateTest.scala
@@ -1,14 +1,22 @@
-import org.scalatest.FunSuite
-
+import main.scala._
+import org.scalatest.FlatSpec
 /**
  * Created by Andrei on 28/01/15.
  */
-class StateTest extends FunSuite {
-  test("This is a simple test") {
-    assert(true)
+
+class StateTest extends FlatSpec {
+  trait StandardState {
+    val state = new State(new StandardTileBag())
+    val startTile = SimpleTile("D", RoadEdge(0, 0, 1), RoadEdge(1, 0, 0), GrassEdge(1), CityEdge(0))
   }
 
-  test("Failing test") {
-    assert(false)
+  "A Standard Package State" should "have one tile when initialised" in new StandardState {
+    val collection = state.getBoard()
+    assert(collection.size == 1)
+  }
+
+  it should "have the starting tile when initialised" in new StandardState {
+    val tile : Option[Tile] = state.getBoard().get((0,0))
+    assert(tile != None)
   }
 }
