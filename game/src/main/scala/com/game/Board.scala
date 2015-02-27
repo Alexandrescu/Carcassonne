acf0e3ae5ac36cea5488b7dd2c22b59cfd222134
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-02-27 12:32:39 +0000

    [Code] GameBoard. NOT tested!
    This is dependent on UnionKeeper, which is not yet commit and Follower
    in Section (not commit)

--- game/src/main/scala/com/game/Board.scala
+++ game/src/main/scala/com/game/Board.scala
@@ -1,11 +1,11 @@
 package com.game
 
-import main.scala.Tile
+import main.scala.{Player, Tile}
 
 trait Board {
   final type Place = (Integer, Integer)
   def get(place : Place) : Option[Tile]
   def getBoard() : Map[Place, Tile]
-  def setMove(tile: Tile, place : Place, tileSection : Section, follower : PlayerFollower) : Boolean
-  def isMove(tile : Tile, place : Place, tileSection : Section, follower : PlayerFollower) : Boolean
+  def setMove(tile: Tile, place : Place, tileSection : TileSection, player : Player) : Boolean
+  def isMove(tile : Tile, place : Place, tileSection : TileSection, follower : Follower) : Boolean
 }
--- game/src/main/scala/com/game/GameBoard.scala
+++ game/src/main/scala/com/game/GameBoard.scala
@@ -1,27 +1,87 @@
 package com.game
 
-import main.scala.Tile
+import main.scala.Direction._
+import main.scala.Direction.Direction
+import main.scala.{Player, Direction, Tile}
 
-import scala.collection.immutable.HashMap
+import scala.collection.mutable._
 
-class GameBoard extends Board{
-  val board : HashMap[Place, Tile] = new HashMap()
 
-  override def get(place : Place): Option[Tile] = {
-    if(board.contains(place))
-      board.get(place)
-    None
+class GameBoard(logic: Logic, unionKeeper : UnionKeeper) extends Board{
+  val BaseSection = unionKeeper.base
+  val board : scala.collection.immutable.HashMap[Place, Tile] = new scala.collection.immutable.HashMap()
+
+  private def canOwn(boardSections: Option[HashSet[BoardSection]]) : Boolean =  boardSections match {
+    case None => true
+    case Some(sections) => sections.forall(unionKeeper.isOwned)
   }
 
-  override def setMove(tile: Tile, place : Place, tileSection : Section, follower: PlayerFollower) : Boolean = {
-    false
+  override def get(place : Place): Option[Tile] = board.get(place)
+
+  def updateBoard(tile: Tile, place : Place, deps: HashMap[TileSection, HashSet[BoardSection]], player: Player, toOwn : TileSection): Unit = {
+    var freeSections = tile.getSections()
+
+    // Union-ing the dependencies
+    deps.foreach{case (tileSection, boardSections) => {
+      if(freeSections.contains(tileSection) == false) {
+        throw new Error("This is not something we have on the tile")
+      }
+      freeSections = freeSections - tileSection
+
+      val newSection = boardSections.foldLeft(BaseSection){case (sectA, sectB) => unionKeeper.union(sectA, sectB)}
+      if(tileSection == toOwn) {
+        unionKeeper.own(newSection, player)
+      }
+      tile.updateSection(tileSection, newSection)
+    }}
+
+    freeSections.foreach(section => {
+      val newSection = unionKeeper.add(section)
+      tile.updateSection(section, newSection)
+    })
   }
 
-  override def getBoard(): Map[Place, Tile] = {
+  override def setMove(tile: Tile, place : Place, toOwn : TileSection, player : Player) : Boolean = {
+    val move = isMove(tile, place)
+    if(move.isEmpty) return false
+    val dependencies = move.get
+
+    if(!canOwn(dependencies.get(toOwn))) return false
+
+    updateBoard(tile, place, dependencies, player, toOwn)
+    true
+  }
+
+  def isMove(tile : Tile, place : Place): Option[HashMap[TileSection, HashSet[BoardSection]]] = {
+    var map : scala.collection.immutable.Map[Direction, Tile] = scala.collection.immutable.HashMap()
+    Direction.values.foreach(direction => getTile(place, direction) match {
+      case Some(tile) => map = map + (direction -> tile)
+      case _ => {}
+    })
+    logic.isMove(tile, map)
+  }
+
+  // This implies that a Tile must include sufficient information to be able to do this
+  // The board could connect with the Logic so that if the logic needs more details it can query the board
+  override def isMove(tile: Tile, place: Place, whereToPlace: TileSection, follower: Follower): Boolean = {
+    true
+  }
+  override def getBoard(): scala.collection.immutable.Map[Place, Tile] = {
     board
   }
 
-  override def isMove(tile: Tile, place: Place, tileSection: Section, follower: PlayerFollower): Boolean = {
-    true
+  implicit def directionToPlace(dir : Direction) : Place = dir match {
+    case Up => (0, 1)
+    case Down => (0, -1)
+    case Left => (-1, 0)
+    case Right => (1, 0)
   }
+  private def add(place1 : Place, place2 : Place): Place = {
+    (place1._1 + place2._1, place1._2 + place2._2)
+  }
+  private def getTile(place : Place, direction : Direction) : Option[Tile] = {
+    get(add(place, direction))
+  }
+
+
 }
