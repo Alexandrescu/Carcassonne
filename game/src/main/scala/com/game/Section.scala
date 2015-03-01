e9a995a94b6f222958d0fc4ed0fd50fa9e602ffe
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-01 17:55:10 +0000

    [Code] Inspired section
    This is exactly what the tile would need to know about the sections.

--- game/src/main/scala/com/game/Section.scala
+++ game/src/main/scala/com/game/Section.scala
@@ -1,5 +1,26 @@
 package com.game
 
-abstract class Section
-case class TileSection(id : Int) extends Section
-case class BoardSection(id : Int) extends Section
+case class Section(id : Int) {
+  var isPlayed : Boolean = false
+}
+
+case class GrassSection(override val id : Int) extends Section(id) {
+  var parent : Option[GrassSection] = None
+  var closedCities : Int = 0
+}
+
+case class CitySection(override val id : Int) extends Section(id) {
+  var openEdges : Int = 0
+  var tileCount : Int = 0
+  var parent : Option[CitySection] = None
+  var adjacentGrass : Set[GrassSection] = Set()
+}
+
+case class RoadSection(override val id : Int) extends Section(id) {
+  var openEdges : Int = 0
+  var tileCount : Int = 0
+  var parent : Option[RoadSection] = None
+}
+case class MonasterySection(override val id : Int) extends Section(id) {
+  var tileCount : Int = 0
+}
