564eb72cd67a5f734ba3e5b79ac13400e4cda317
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-03-07 14:57:29 +0000

    [Refactor] Sections need to be individuals by object separation

--- Game/src/main/scala/com/tile/Section.scala
+++ Game/src/main/scala/com/tile/Section.scala
@@ -1,27 +1,27 @@
 package com.tile
 
-case class Section() {
+class Section{
   var isPlayed : Boolean = false
   def isOwned = ???
 }
 
-case class GrassSection() extends Section() {
+class GrassSection extends Section{
   var parent : Option[GrassSection] = None
   var closedCities : Int = 0
 }
 
-case class CitySection() extends Section() {
+class CitySection extends Section{
   var openEdges : Int = 0
   var tileCount : Int = 0
   var parent : Option[CitySection] = None
   var adjacentGrass : Set[GrassSection] = Set()
 }
 
-case class RoadSection() extends Section() {
+class RoadSection extends Section{
   var openEdges : Int = 0
   var tileCount : Int = 0
   var parent : Option[RoadSection] = None
 }
-case class MonasterySection() extends Section() {
+class MonasterySection extends Section {
   var tileCount : Int = 0
 }
