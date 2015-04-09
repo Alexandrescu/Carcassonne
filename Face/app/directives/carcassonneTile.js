73236c2023b3431a6797aa1eaf16821dac6359d6
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-09 20:28:13 +0300

    [Code] Carcassonne Tile

---
+++ Face/app/directives/carcassonneTile.js
@@ -0,0 +1,37 @@
+var carcassonne = angular.module('carcassonne');
+
+carcassonne.directive('carcassonneTile', ['$d3', function($d3) {
+  return {
+    restrict: 'E',
+    scope: {
+      tileSize: '=',
+      tile: '='
+    },
+    link: function(scope, element, attrs) {
+      var tileLetter = angular.lowercase(scope.tile);
+      var tileSize = scope.tileSize;
+
+      var tile = $d3.select(element[0]).append('svg')
+          .attr('width', tileSize)
+          .attr('height', tileSize)
+        .append('image')
+          .attr('width', tileSize)
+          .attr('height', tileSize)
+          .attr('xlink:href', '/tiles/' + tileLetter + 'Tile.png');
+
+      var rotation = 0;
+
+      element.bind('mousedown', function(event) {
+        console.log(event);
+        event.preventDefault();
+
+        rotation = (rotation + 90) % 360;
+        tile.attr('transform', 'rotate(' + rotation + ' ' + (tileSize / 2) + ' ' + (tileSize / 2) + ')');
+      });
+
+      element.on('contextmenu', function(event) {
+        event.preventDefault();
+      })
+    }
+  }
+}]);
