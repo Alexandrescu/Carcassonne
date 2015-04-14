9f6e7535c2dfe28d55af1f8f453a9e0981997de1
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-14 16:53:26 +0300

    [Code] Updated view to handle to protocol

--- Face/app/controllers/MainCtrl.js
+++ Face/app/controllers/MainCtrl.js
@@ -1,6 +1,10 @@
 var carcassonne = angular.module('carcassonne');
 
 carcassonne.controller('MainCtrl', ['$scope', '$socket', function($scope, $socket) {
+  $scope.myRoom = {
+    roomName : '',
+    joined : false
+  };
   var socket = $socket.io('http://localhost:1337');
 
   socket.on('availableRooms', function (data) {
@@ -18,24 +22,33 @@ carcassonne.controller('MainCtrl', ['$scope', '$socket', function($scope, $socke
     }
   });
 
+  // Stop the progress circle
   socket.on('connect', function () {
-    // Stop the progress circle
     $scope.progressFinish = true;
-
   });
 
+  // Start the progress circle
   socket.on('disconnect', function() {
+    $scope.myRoom.joined = false;
     $scope.progressFinish = false;
   });
 
   //Joining room
   socket.on('roomConnected', function(data) {
-    $scope.myRoom = data.roomName;
-    $scope.roomConnected = true;
+    console.log("ROOM CONNECTED");
+    console.log(data);
+    $scope.myRoom = {
+      roomName :data.roomName,
+      joined: true
+    };
+    if(data.slots) {
+      $scope.slots = data.slots.sort(function(a, b) { return a.slot - b.slot});
+    }
     $scope.creatingRoom = false;
   });
 
   socket.on('roomUpdate', function(room) {
+    $scope.slots = room.slots.sort(function(a, b) { return a.slot - b.slot});
   });
 
   // Join Room request
@@ -46,19 +59,54 @@ carcassonne.controller('MainCtrl', ['$scope', '$socket', function($scope, $socke
   // Create room and join
   $scope.createRoom = function(roomName) {
     if(roomName && roomName != '') {
-      $scope.myRoom = roomName;
       $scope.creatingRoom = true;
       socket.emit('addRoom', {roomName: roomName});
     }
   };
   $scope.id = {};
   $scope.chooseNickname = function() {
-    if($scope.id.nickname && $scope.id.nickname != '') {
+    console.log($scope.id.disabled);
+    if($scope.id.disabled) {
+      console.log("HERE");
+      $scope.id.disabled = false;
+      //UPDATE ROOM
+    }
+    else if($scope.id.nickname && $scope.id.nickname != '') {
       $scope.id.disabled = true;
     }
+  };
+
+  $scope.joinSlot = function(index) {
+    $scope.myRoom.slot = index;
+    var data = {
+      roomName: $scope.myRoom.roomName,
+      playerName: $scope.id.nickname,
+      slot: index,
+      isAI: false
+    };
+    console.log("addPlayer event");
+    console.log(data);
+    socket.emit('addPlayer', data);
+  };
+
+  $scope.removeSlot = function(index) {
+    var data = {
+      roomName: $scope.myRoom.roomName,
+      playerName: $scope.id.nickname,
+      slot: index,
+      isAI: false
+    };
+    if(index === $scope.myRoom.slot) {
+      delete $scope.myRoom.slot;
+    }
+    socket.emit('removePlayer', data);
   };
 
   $scope.$on('$destroy', function() {
     socket.removeAllListeners();
   });
+
+  // Slot section
+  var slotNumber = 6;
+  $scope.slots = new Array(slotNumber);
 }]);
