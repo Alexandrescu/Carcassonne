c8d85cdfc28f6039862b0f7231f298ee929e2e21
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-19 16:53:50 +0300

    [#10] Changed Option[Section] to List[Option[Section]]

--- Game/src/main/scala/com/board/Move.scala
+++ Game/src/main/scala/com/board/Move.scala
@@ -5,4 +5,4 @@ import com.game.Player
 import com.tile.{Section, Tile}
 
 class Move(val tile : Tile, val place : (Int, Int), val toOwnFromTile : Option[Section], val player : Player)
-class PossibleMove(val direction : Direction, val place : (Int, Int), val toOwnFromTile : Option[Section])
+class PossibleMove(val direction : Direction, val place : (Int, Int), val toOwnFromTile : List[Option[Section]])
