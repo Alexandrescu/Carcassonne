6993ecbd2de610b325530a5ed6db6714c5074f75
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-20 08:50:16 +0300

    [#12] Commit fix

---
+++ Game/src/main/scala/com/server/ServerScala.scala
@@ -0,0 +1,55 @@
+package com.server
+
+import com.corundumstudio.socketio._
+import com.corundumstudio.socketio.listener.DataListener
+import com.server.events.{GameEvents, RoomEvents}
+import com.server.json.{Room, RoomList}
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
+
+class ServerScala {
+  val logger = Logger(LoggerFactory.getLogger("Carcassonne Server"))
+
+  val config = new Configuration()
+  //config.setHostname("localhost")
+  config.setPort(1337)
+
+  val server : SocketIOServer = new SocketIOServer(config)
+
+  def start() = server.start()
+  def stop() = server.stop()
+
+  val rooms = new RoomSet()
+
+  server.addListeners(new RoomEvents(rooms))
+
+  server.addEventListener("startGame", classOf[Room], new DataListener[Room]() {
+    override def onData(client: SocketIOClient, data: Room, ackRequest: AckRequest): Unit = {
+      val namespace = server.addNamespace('/' + data.roomName)
+      namespace.addListeners(new GameEvents(new PlayerState(rooms.getRoomDetails(data.roomName))))
+      availableGames(client)
+    }
+  })
+
+  server.addEventListener("stopGame", classOf[Room], new DataListener[Room]() {
+    override def onData(client: SocketIOClient, data: Room, ackRequest: AckRequest): Unit = {
+      server.removeNamespace(data.roomName)
+      availableGames(client)
+    }
+  })
+
+  server.addEventListener("getGame", classOf[Object], new DataListener[Object]() {
+    override def onData(client: SocketIOClient, data: Object, ackRequest: AckRequest): Unit = {
+      availableGames(client)
+    }
+  })
+
+  def availableGames(client : SocketIOClient): Unit = {
+    val roomList = new RoomList
+    roomList.setTheRooms((for(space <- server.getAllNamespaces) yield new Room(space.getName)).toList.toList)
+    client.getNamespace.getBroadcastOperations.sendEvent("availableGames", roomList)
+  }
+
+}
