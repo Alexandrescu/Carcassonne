acf7bce8004afd97d796e6afdc22c7a0b4d19be3
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-25 13:41:23 +0100

    [Code] Updated Road Section

--- Game/src/main/scala/com/tile/RoadSection.scala
+++ Game/src/main/scala/com/tile/RoadSection.scala
@@ -1,7 +1,5 @@
 package com.tile
 
-import com.game.Player
-
 class RoadSection(override val frontEndId : Int) extends Section(frontEndId){
   private var _openEdges : Int = 0
   private var _tileCount : Int = 1
@@ -39,31 +37,24 @@ class RoadSection(override val frontEndId : Int) extends Section(frontEndId){
   }
 
   override def isOwned: Boolean = {
-    if(_parent.isEmpty) return owners.nonEmpty
+    if(_parent.isEmpty) return followers.nonEmpty
     findRoot().isOwned
   }
 
-  override def addOwners(newOwners: Map[Player, Int]): Unit = {
+  override def addFollowers(newFollowers: Set[Follower]): Unit = {
     val root = findRoot()
-
-    newOwners.foreach{ case (player, followers) =>
-      val newFollowers = root._owners.getOrElse(player, 0) + followers
-      if(newFollowers != 0) {
-        root._owners -= player
-        root._owners += (player -> newFollowers)
-      }
-    }
+    root._followers = root._followers.union(newFollowers)
   }
-  override def owners: Map[Player, Int] = findRoot()._owners
+  override def followers: Set[Follower] = findRoot()._followers
 
   override def updateClose(): Unit = {
     val root = findRoot()
     if(root.openEdges == 0) {
       //closed
-      var maxFollowers = 0
-      root._owners.foreach{case (player, followers) => maxFollowers = Math.max(followers, maxFollowers)}
-      root._owners.filter(p => p._2 == maxFollowers).foreach{case (player, _) => player.addPoints(root.tileCount())}
-      root._owners.foreach{case (player, followers) => player.addFollowers(followers)}
+      for(player <- majority(root._followers)) {
+        player.addPoints(root.tileCount())
+      }
+      removeFollowers(root._followers)
     }
   }
 }
