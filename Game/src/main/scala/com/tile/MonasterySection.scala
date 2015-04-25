6b0171c318cb4e8f121fc858a2a58f132fff9fc6
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-25 13:35:17 +0100

    [Code] Updated Monastery Section

--- Game/src/main/scala/com/tile/MonasterySection.scala
+++ Game/src/main/scala/com/tile/MonasterySection.scala
@@ -1,7 +1,5 @@
 package com.tile
 
-import com.game.Player
-
 class MonasterySection(override val frontEndId : Int) extends Section(frontEndId) {
   private var tileCount : Int = 0
 
@@ -13,24 +11,25 @@ class MonasterySection(override val frontEndId : Int) extends Section(frontEndId
     tileCount -= 1
   }
 
-  override def isOwned: Boolean = owners.nonEmpty
+  override def isOwned: Boolean = followers.nonEmpty
 
-  override def addOwners(newOwners: Map[Player, Int]): Unit = {
+  override def addFollowers(newOwners: Set[Follower]): Unit = {
     if(newOwners.size > 1)
       throw new Exception("Can't have more than one owner on Monastery")
-    _owners = newOwners
+    _followers = newOwners
   }
 
-  override def owners: Map[Player, Int] = _owners
+  override def followers: Set[Follower] = _followers
 
   override def updateClose(): Unit = {
     if(tileCount == 0) {
       var flagIterated = false
-      owners.foreach{case (player, followers) =>
+      for(follower <- _followers) {
         if(flagIterated) throw new Error("Monastery should only have one owner")
-
         flagIterated = true
-        player.addPoints(9)
+
+        follower.player.addPoints(9)
+        follower.take()
       }
     }
   }
