12ee11af0e3a6dbdd2eccf02667e3ae7bafb5f9f
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-25 12:57:39 +0100

    [Code] Defined follower #10

---
+++ Game/src/main/scala/com/tile/Follower.scala
@@ -0,0 +1,15 @@
+package com.tile
+
+import com.game.Player
+
+// Player is who's follower this is.
+class Follower(val player : Player) {
+  private var _section : Option[Section] = None
+
+  def section() : Option[Section] = _section
+  def section_= (newSection : Section) : Unit = {
+    _section = Some(newSection)
+  }
+
+  def isPlaced : Boolean = _section.isDefined
+}
