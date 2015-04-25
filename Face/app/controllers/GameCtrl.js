daf19088fe35d46fe58668a8e70c6a0211869ff7
Alexandrescu <andrei.alexandrescu@oriel.ox.ac.uk>
2015-04-25 10:37:24 +0100

    [#14] Game controller fix

--- Face/app/controllers/GameCtrl.js
+++ Face/app/controllers/GameCtrl.js
@@ -1,8 +1,4 @@
 var carcassonne = angular.module('carcassonne');
 
 carcassonne.controller('GameCtrl', ['$scope', '$socket', '$location', function($scope, $socket, $location) {
-  $scope.move = 0;
-  $scope.changeMove = function() {
-    $scope.move += 1;
-  }
 }]);
